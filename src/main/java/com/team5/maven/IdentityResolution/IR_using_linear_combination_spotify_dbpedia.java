ee20e31a308e3a1723deac6660f138928446909b
HDDung <cs2014_dung.hd@student.vgu.edu.vn>
2020-12-03 23:43:16 +0100

    [DUNG] Spotify_dbpedia now using remove bracket and dash

--- src/main/java/com/team5/maven/IdentityResolution/IR_using_linear_combination_spotify_dbpedia.java
+++ src/main/java/com/team5/maven/IdentityResolution/IR_using_linear_combination_spotify_dbpedia.java
@@ -11,6 +11,8 @@ import com.team5.maven.IdentityResolution.comparators.SongArtistsComparatorToken
 import com.team5.maven.IdentityResolution.comparators.SongNameComparatorEqual;
 import com.team5.maven.IdentityResolution.comparators.SongNameComparatorJaccard;
 import com.team5.maven.IdentityResolution.comparators.SongNameComparatorLevenshtein;
+import com.team5.maven.IdentityResolution.comparators.SongNameComparatorRemoveBrackets;
+import com.team5.maven.IdentityResolution.comparators.SongNameComparatorRemoveBracketsAndDash;
 import com.team5.maven.IdentityResolution.comparators.SongNameComparatorTokenJaccard;
 import com.team5.maven.IdentityResolution.model.Song;
 import com.team5.maven.IdentityResolution.model.SongXMLReader;
@@ -31,7 +33,7 @@ import de.uni_mannheim.informatik.dws.winter.utils.WinterLogManager;
 
 public class IR_using_linear_combination_spotify_dbpedia {
 
-	private static final Logger logger = WinterLogManager.activateLogger("default");
+	private static final Logger logger = WinterLogManager.activateLogger("traceFile");
 	
     public static void main( String[] args ) throws Exception
     { 
@@ -51,11 +53,11 @@ public class IR_using_linear_combination_spotify_dbpedia {
 
 		// create a matching rule
 		LinearCombinationMatchingRule<Song, Attribute> matchingRule = new LinearCombinationMatchingRule<>(
-				0.7);
+				0.65);
 		matchingRule.activateDebugReport("data/output/debugResultsMatchingRule_spotify_dbpedia.csv", 1000, gsTest);
 		
 		// add comparators
-		matchingRule.addComparator(new SongNameComparatorTokenJaccard(), 0.6);
+		matchingRule.addComparator(new SongNameComparatorRemoveBracketsAndDash(), 0.6);
 		matchingRule.addComparator(new SongArtistCustomizedComparator(), 0.4);
 		
 
