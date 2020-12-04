972bc797c45d1ae55aca25604f839d5f36531241
HDDung <cs2014_dung.hd@student.vgu.edu.vn>
2020-12-04 21:33:32 +0100

    [DUNG] Metadata Constant

---
+++ src/main/java/com/team5/maven/common/MetaData.java
@@ -0,0 +1,7 @@
+package com.team5.maven.common;
+
+public final class MetaData {
+	public static final String SPOTIFY_DATE 	= "2020-10-01";
+	public static final String DBPEDIA_DATE 	= "2020-10-25";
+	public static final String MUSICBRAINZ_DATE = "2017-07-01";
+}
