c3e0e6b966a8413a3da4508b57636cf5748254c2
HDDung <cs2014_dung.hd@student.vgu.edu.vn>
2020-12-04 05:07:56 +0100

    [DUNG] Duration minimum

---
+++ src/main/java/com/team5/maven/DataFusion/fusers/DurationFuserMin.java
@@ -0,0 +1,41 @@
+package com.team5.maven.DataFusion.fusers;
+
+import com.team5.maven.DataFusion.fusers.utility.Minimum;
+import com.team5.maven.IdentityResolution.model.Song;
+
+import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+
+public class DurationFuserMin extends
+		AttributeValueFuser<Double, Song, Attribute>{
+
+	public DurationFuserMin() {
+		super(new Minimum<Song, Attribute>());
+	}
+
+
+	@Override
+	public boolean hasValue(Song record, Correspondence<Attribute, Matchable> correspondence) {
+		return record.hasValue(Song.DURATION);
+	}
+	
+	@Override
+	public Double getValue(Song record, Correspondence<Attribute, Matchable> correspondence) {
+		return record.getDuration();
+	}
+
+	@Override
+	public void fuse(RecordGroup<Song, Attribute> group, Song fusedRecord, Processable<Correspondence<Attribute, Matchable>> schemaCorrespondences, Attribute schemaElement) {
+		FusedValue<Double, Song, Attribute> fused = getFusedValue(group, schemaCorrespondences, schemaElement);
+		fusedRecord.setDuration(fused.getValue());
+		fusedRecord.setAttributeProvenance(Song.DURATION,
+				fused.getOriginalIds());
+	}
+
+}
